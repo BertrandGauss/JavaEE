4607720e4de3d35b5a069c3a90c59b7ea83e2e16
Dmitvna <83126650+Dmitvna@users.noreply.github.com>
2021-07-12 13:15:05 +0800

    数据库提交

--- src/main/java/cn/health/mapper/User_ExerciseMapper.java
+++ src/main/java/cn/health/mapper/User_ExerciseMapper.java
@@ -2,13 +2,15 @@ package cn.health.mapper;
 
 
 import cn.health.domain.User_Exercise;
+import org.apache.ibatis.annotations.Param;
+
 import java.util.Date;
 
 public interface User_ExerciseMapper {
     void add(User_Exercise user_exercise);
 
-    void updatetotal_calorie(Long total_calorie);
+    void updatetotal_calorie(@Param("total_calorie") Integer total_calorie, @Param("date") Date date, @Param("user_id") Integer user_id);
 
-    User_Exercise selectByIdDate(Integer user_id, Date date);  //根据时间用户id查找信息
+    User_Exercise selectByIdDate(@Param("user_id") Integer user_id, @Param("date") Date date);  //根据时间用户id查找信息
 
 }
