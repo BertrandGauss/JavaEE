31c0fc227ced938e2d2c8f2cea17ddf5c3b1c9e4
HHGRR <64680576+HHGRR@users.noreply.github.com>
2021-07-12 11:32:21 +0800

    Update User_Height_WeightMapper.java

--- src/main/java/cn/health/mapper/User_Height_WeightMapper.java
+++ src/main/java/cn/health/mapper/User_Height_WeightMapper.java
@@ -10,6 +10,6 @@ public interface User_Height_WeightMapper {
     User_Height_Weight selectByIdDate(@Param("user_id")Integer user_id, @Param("date")Date date); //根据用户id找到相应的信息
     List<User_Height_Weight> selecttotal(Integer user_id);//根据用户id返回该用户所有信息
     void add(User_Height_Weight user_height_weight);//向user_height_weight数据库增加一条信息
-    void update_height_weight(@Param("date")Date date,@Param("height")Integer height,@Param("weight")Integer weight);//根据时间和id找到某用户的一条信息，
+    void update_height_weight(@Param("user_id")Integer user_id,@Param("date")Date date,@Param("height")Integer height,@Param("weight")Integer weight);//根据时间和id找到某用户的一条信息，
                                                                                                                         // 更新该信息中的身高和体重数据
 }
