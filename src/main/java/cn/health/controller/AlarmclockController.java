34777e361999ab17c6e819dbb1eb3d5a25615c79
BertrandGauss <673809859@qq.com>
2021-07-17 17:39:10 +0800

    后端闹钟添加修改bug

--- src/main/java/cn/health/controller/AlarmclockController.java
+++ src/main/java/cn/health/controller/AlarmclockController.java
@@ -23,10 +23,14 @@ public class AlarmclockController {
 
     //添加警告记录
     @RequestMapping(value = "/addalarmclock", method = {RequestMethod.POST})
-    private void addAlarmclock(@RequestBody Alarmclock alarmclock) {
+    private JSONObject addAlarmclock(@RequestBody Alarmclock alarmclock) {
         Integer id=(int)httpServletRequest.getSession().getAttribute("LOGIN_USER");
         alarmclock.setUser_id(id);
         alarmclockService.addAlarmclock(alarmclock);
+        JSONObject json = new JSONObject();
+        json.put("code",0);
+        json.put("msg","添加闹钟成功");
+        return json;
     }
 
     @RequestMapping(value = "/showallalarm", method = {RequestMethod.GET})
